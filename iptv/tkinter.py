77f9055b3fbde389886b03292ba44fcc00b4f6ba
Onur Ağtaş <agtasonur@gmail.com>
2017-03-17 02:45:01 +0300

    add selector

--- iptv/tkinter.py
+++ iptv/tkinter.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from PIL import Image
 from PIL import ImageTk
 import Tkinter as tki
+from tkFileDialog import askopenfilename
 import threading
 import datetime
 import imutils
@@ -21,6 +22,7 @@ class tkinter:
 		self.thread = None
 		self.state = False
 		self.stopEvent = None
+		self.path = "/home/onuragtas/iptv/tv_channels_kalidas.m3u"
 
 		# initialize the root window and image panel
 		self.root = tki.Tk()
@@ -32,7 +34,6 @@ class tkinter:
 		sys.setdefaultencoding('utf-8')
 		# create a button, that when pressed, will take the current
 		# frame and save it to file
-		
 		self.btn = tki.Button(self.root, text="SQL", height=1,
 			command=self.convert)
 		self.btn.pack(side="bottom", fill="both", expand="yes", padx=5,
@@ -43,6 +44,11 @@ class tkinter:
 		
 		self.textip = tki.Text(self.root, height=10)
 		self.textip.pack(side="bottom", fill="both", padx=10, pady=10)
+		
+		self.btn = tki.Button(self.root, text="Select File", height=1,
+			command=self.openfile)
+		self.btn.pack(side="bottom", fill="both", expand="yes", padx=5,
+			pady=10)
 			
 		#self.stopEvent = threading.Event()
 		#self.thread = threading.Thread(target=self.videoLoop, args=())
@@ -52,6 +58,10 @@ class tkinter:
 		# set a callback to handle when the window is closed
 		self.root.wm_title("IPTV Convert to SQL")
 		self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
+	def openfile(self):
+		filename = askopenfilename()
+		file = open(filename, "r");
+		self.textip.insert(1.0, file.read())
 	def toggle_fullscreen(self, event=None):
 		self.state = not self.state
 		self.root.attributes("-fullscreen", self.state);
@@ -63,15 +73,15 @@ class tkinter:
 
 	def convert(self):
 		iptvtext = self.textip.get(1.0, tki.END);
-		iptvtext = iptvtext.replace("#EXTINF:0,", "").replace("#EXTM3U", "").replace(".ts", ".ts | ");
+		iptvtext = iptvtext.replace("#EXTINF:0,", "").replace("#EXTINF:-1,", "").replace("#EXTM3U", "").replace(".ts", ".ts | ");
 		array = iptvtext.split("|");
 		self.sql = "";
 		for item in array:
 			try:
 				channel = item.split("http")
 				name = channel[0].rstrip().lstrip();
-				url = "http"+channel[1];
-				self.sql = self.sql+"INSERT INTO channels (title, rtmp, image, cat_id, sira)VALUES('"+name+"', '"+url+"', '','0', '0');";
+				url = "http"+channel[1].rstrip();
+				self.sql = self.sql+"INSERT INTO channels (title, rtmp, image, cat_id, sira)VALUES('"+name+"', '"+url+"', '','0', '0');\n";
 			except IndexError:
 				print("index hatası");
 		self.writeSQL();
